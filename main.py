fc6a8baf3ba0516638442915fb05016c4253559b
jasonc8901 <jasonc8901@gmail.com>
2021-10-10 23:41:32 -0500

    4d arrays are annoying

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import matplotlib
 import numpy as np
 from matplotlib import pyplot as plt
+import scipy.io
 
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    sig = 1 / (1 + np.exp(-x))
+    sig = np.minimum(sig, 0.95)  # Set upper bound
+    sig = np.maximum(sig, 0.0001)  # Set lower bound
+    return sig
 
 
 def sigmoid_der(x):
     return sigmoid(x)*(1-sigmoid(x))
 
 
-if __name__ == '__main__':
+def relu(x):
+    return max(0.0, x)
+
+
+def first_nn():
     feature_set = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]])
 
     labels = np.array([[1, 0, 0, 1, 1]])
@@ -22,7 +30,10 @@ if __name__ == '__main__':
     bias = np.random.rand(1)
     lr = 0.05
 
-    for epoch in range(5000):
+    es = []
+    eps = []
+
+    for epoch in range(500):
         inputs = feature_set
 
         # feedforward step1
@@ -48,13 +59,60 @@ if __name__ == '__main__':
         for num in z_delta:
             bias -= lr * num
 
-    single_point = np.array([0, 1, 0])
+        es.append(error.sum())
+        eps.append(epoch)
+
+    single_point = np.array([0, 1, 0])  # network test
     result = sigmoid(np.dot(single_point, weights) + bias)
     print(result)
     points = np.linspace(-10, 10, 20)
 
-    # plt.plot(points, sigmoid(points), c="r")
+    plt.plot(es, eps, c="b")
+    # plt.plot(points, sigmoid(points), c="b")
 
     plt.show(block=True)
     plt.interactive(False)
+
+
+if __name__ == '__main__':
+    # first_nn()
+    mat = scipy.io.loadmat('training.mat')
+    feature_set = mat['X']
+    s = feature_set.shape
+    feature_set = feature_set.reshape(s[3], s[0] * s[1] * s[2])[:50]
+
+    labels = mat['y'][:50]
+
+    np.random.seed(1000)
+    weights = np.random.rand(3072, 1)
+    bias = np.random.rand(1)
+    lr = 0.05
+
+    for epoch in range(500):
+        inputs = feature_set
+
+        # feedforward step1
+        XW = np.dot(feature_set, weights) + bias
+
+        # feedforward step2
+        z = relu(XW)
+
+        # backpropagation step 1
+        error = z - labels
+
+        print(error.sum())
+
+        # backpropagation step 2
+        dcost_dpred = error
+        dpred_dz = sigmoid_der(z)
+
+        z_delta = dcost_dpred * dpred_dz
+
+        inputs = feature_set.T
+        weights -= lr * np.dot(inputs, z_delta)
+
+        for num in z_delta:
+            bias -= lr * num
+
+
 
